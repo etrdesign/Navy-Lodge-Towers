d06f0efe71047a204dbafbec4f6499c54c686c55
etrdesign <63577744+etrdesign@users.noreply.github.com>
2021-02-11 20:39:29 +0900

    first commit

---
+++ data.js
@@ -0,0 +1,399 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-of-tower",
+      "name": "Front of Tower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": -3.1327225396867497,
+        "pitch": 0.024558215534057126,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-front-entrance",
+      "name": "Front Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": 2.8548599907621632,
+        "pitch": 0.02455821553406423,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-elevator",
+      "name": "Elevator",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": 3.0370539914275536,
+        "pitch": 0.08731809967665072,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-inside-elevator",
+      "name": "Inside Elevator",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-room-lobby",
+      "name": "Room Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": -0.09225311760541999,
+        "pitch": -0.0491164310681107,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-front-room-entrance",
+      "name": "Front Room Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": -3.116789320009575,
+        "pitch": 0.0005403467757929548,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0779784626551336,
+          "pitch": 0.37741148065354935,
+          "rotation": 0,
+          "target": "8-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": -2.8884922126452963,
+        "pitch": 0.0573025029128047,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.09840005379802719,
+          "pitch": 0.053983170355206056,
+          "title": "Stove",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4244963040331236,
+          "pitch": 0.11208330774359965,
+          "title": "Referigerator&nbsp;",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-pantry",
+      "name": "Pantry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": 2.655393790534248,
+        "pitch": -0.06548857475748093,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": -0.19396481192884707,
+        "pitch": -0.00818607184468334,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-balcony",
+      "name": "Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": -1.5571326784635655,
+        "pitch": 0.03794611023962702,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": 2.8228320049312305,
+        "pitch": -0.05730250291279404,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-laundry-room",
+      "name": "Laundry Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": 0.00997331001139834,
+        "pitch": -0.060031193527699145,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": 0.00008287528926054222,
+        "pitch": 0,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-master-bedroom",
+      "name": "Master Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-2nd-bedroom",
+      "name": "2nd Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": 0.28734375108995636,
+        "pitch": 0.15698326750243297,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-back-of-tower",
+      "name": "Back of Tower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 313.5,
+      "initialViewParameters": {
+        "yaw": -1.3064438358022894,
+        "pitch": -0.22103702347607168,
+        "fov": 1.948285099035228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
